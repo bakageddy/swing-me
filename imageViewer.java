b39e2e61037c74e0c5ee61bb3a2950609eefe5b5
Dinesh Kumar <gdineshda@gmail.com>
2022-09-05 00:45:03 +0530

    Organize Imports

--- imageViewer.java
+++ imageViewer.java
@@ -4,8 +4,10 @@ import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.IOException;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,6 +31,7 @@ public class imageViewer {
         File files[] = dir.listFiles();
         List<File> list = Arrays.asList(files);
 
+        // Same as the below code snippet
         list.removeIf((file) -> {
             return (!file.getName().endsWith(".jpg")
                     && (!file.getName().endsWith(".png"))
